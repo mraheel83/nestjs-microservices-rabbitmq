d0bd1a05a664a184828449b89ac3d958b037ddeb
Muhammad Raheel <mraheel83@gmail.com>
2022-11-03 23:17:14 +0100

    Update database.module.ts

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './mongoose-config.service';
 
 @Module({
   imports: [
-    /**
-     * Reason to use forRoot because forRootAsync wasn't able to connect
-     * to mongo replica-set asynchronically.
-     */
-    MongooseModule.forRoot('mongodb://root:password123@mongodb-primary:27017/'),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
   ],
 })
 export class DatabaseModule {}
